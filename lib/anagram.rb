671b87c693e30e8bc4376225535f7e5d05c88b85
Fer Hernandez <fh.1111@gmail.com>
2018-11-26 23:11:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
 
   def match(anagrams)
     #puts "#{@word.reverse.match(anagrams)}"
-    puts "#{@word.match(anagrams)}" 
+    puts "#{w(enlists google inlets banana)}" 
     #match(anagrams)
   end
 
